7dddf494350826acd3e0e1538daec98f19b4a1fc
Pascalrjt <pascal.rogerjt@gmail.com>
2023-05-17 14:56:29 +0700

    frfr

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('content')
+
+<div class="main-image">
+    <div class="container">
+        <h1><span>Pethub Adoption<br></span></h1>
+        <div class="paddingText">
+            <span class="cen">Petflix and chill<br></span>
+        </div>
+        <br>
+        <a class="button" href="/animals">Adopt an Angel Now!</a>
+    </div>
+</div>
+
+@endsection
