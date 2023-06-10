d48eb01e3103eb0b5f9b953ec7af9c20fbee5ace
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2023-06-10 15:36:48 +0700

    ril

---
+++ app/Http/Controllers/AnimalsController.cs
@@ -0,0 +1,154 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using YourAppName.Models;
+using YourAppName.Data;
+
+namespace YourAppName.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnimalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Animals
+        public async Task<IActionResult> Index()
+        {
+            var animals = await _context.Animals.ToListAsync();
+            return View(animals);
+        }
+
+        // GET: Animals/Create
+        public IActionResult Create()
+        {
+            var centers = _context.Centers.ToList();
+            return View(centers);
+        }
+
+        // POST: Animals/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name, Breed, Age, CenterId, Desc, Image")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+
+        // GET: Animals/Show/5
+        public async Task<IActionResult> Show(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            return View(animal);
+        }
+
+        // GET: Animals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            var centers = _context.Centers.ToList();
+            ViewData["Centers"] = centers;
+
+            return View(animal);
+        }
+
+        // POST: Animals/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Breed, Age, CenterId")] Animal animal)
+        {
+            if (id != animal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalExists(animal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            
+            var centers = _context.Centers.ToList();
+            ViewData["Centers"] = centers;
+
+            return View(animal);
+        }
+
+        // GET: Animals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // POST: Animals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+            _context.Animals.Remove(animal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _context.Animals.Any(e => e.Id == id);
+        }
+    }
+}
